66a06980d62f9973cd8bfcaf470c8da2f50d1119
Shimberly <kimmy_11_07@hotmail.com>
2018-11-12 06:26:03 -0500

    empezando pantalla login

--- app/src/main/java/com/example/redes/myapplication/MainActivity.java
+++ app/src/main/java/com/example/redes/myapplication/MainActivity.java
@@ -6,11 +6,14 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.util.Log;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-
+    String tag = "LifeCycleEvents";
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -35,10 +38,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        System.out.print("Hola estoy en Create");
+        Log.d(tag, "Hola estoy en Create");
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    public void onStart()
+    {
+        super.onStart();
+        Log.d(tag, "Hola estoy en Start");
+    }
+    public void onRestart()
+    {
+        super.onRestart();
+        Log.d(tag, "Hola estoy en Restart");
+    }
+    public void onResume()
+    {
+        super.onResume();
+        Log.d(tag, "Hola estoy en Resume");
+
+    }
+    public void onPause()
+    {
+        super.onPause();
+        Log.d(tag, "Hola estoy en Pause");
+    }
+    public void onStop()
+    {
+        super.onStop();
+        System.out.print("Hola estoy en Stop");
+    }
+    public void onDestroy()
+    {
+        super.onDestroy();
+        System.out.print("Hola estoy en Destroy");
     }
 
 }
